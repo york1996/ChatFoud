5aca9051696efcb43c2105a37a081044c592ffcc
WhichPath <1023332873@qq.com>
2024-05-15 01:26:28 +0800

    Changes to be committed: 	modified:   core/check_foud_content.py

--- core/check_foud_content.py
+++ core/check_foud_content.py
@@ -1,4 +1,5 @@
 from llm.qwen import generate_response
+from transformers import pipeline
 
 def is_foud_question(content):
     messages = [
@@ -7,3 +8,9 @@ def is_foud_question(content):
     ]
     resp_str = generate_response(messages)
     return resp_str.lower() == "true"
+
+def is_foud_question_faster(content):
+    # https://huggingface.co/WhichPath/gte-small-zh-foud-cls
+    classifier = pipeline("text-classification", model="WhichPath/gte-small-zh-foud-cls")
+    result = classifier(content)
+    return result[0]['label'] == 'TRUE'
