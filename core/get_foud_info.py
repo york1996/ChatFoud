5fb439c7e80075d38209a31a0eb38b46626fae2b
rachelhe <153996854+rachel087@users.noreply.github.com>
2024-05-16 23:33:27 +0800

    Update get_foud_info.py

--- core/get_foud_info.py
+++ core/get_foud_info.py
@@ -1,12 +1,5 @@
-from core.foud_info import FoudInfo
+from core.search import process_input
 
 def get_foud_info(foud_name):
     # TODO: 获取基金信息
-    return FoudInfo(simple_name="西部利得量化成长混合A", 
-                    full_name="西部利得量化成长混合型发起式证券投资基金A类", 
-                    type="混合型", 
-                    code="000006",
-                    manager="西部利得基金管理有限公司",
-                    custodian="中国农业银行股份有限公司",
-                    manager_fee_rate="1.2%",
-                    custodian_fee_rate="0.1%")
+    return process_input(foud_name)
